9040776a739a4548aba3d59283272c317c0921d4
Mark Allen Ramirez <markallenramirez@gmail.com>
2022-11-15 14:13:54 +0800

    Add jQuery example

--- README.md
+++ README.md
@@ -1,25 +1,9 @@
-# Product/Platform - Task
+<!-- default badges list -->
+![](https://img.shields.io/endpoint?url=https://codecentral.devexpress.com/api/v1/VersionRange/555418630/22.1.6%2B)
+[![](https://img.shields.io/badge/Open_in_DevExpress_Support_Center-FF7200?style=flat-square&logo=DevExpress&logoColor=white)]()
+[![](https://img.shields.io/badge/📖_How_to_use_DevExpress_Examples-e9f6fc?style=flat-square)](https://docs.devexpress.com/GeneralInformation/403183)
+<!-- default badges end -->
+# DevExtreme - How to create a date range picker
+This example demonstrates how to create a simple date range picker using [DropDownBox](https://js.devexpress.com/Documentation/Guide/UI_Components/DropDownBox/Getting_Started_with_DropDownBox/) and [Calendar](https://js.devexpress.com/Documentation/Guide/UI_Components/Calendar/Getting_Started_with_Calendar/).
 
-This is the repository template for creating new examples. Describe the solved task here.
-
-Put a screenshot that illustrates the result here.
-
-Then, add implementation details (steps, code snippets, and other technical information in a free form), or add a link to an existing document with implementation details. 
-
-## Files to Review
-
-- link.cs (VB: link.vb)
-- link.js
-- ...
-
-## Documentation
-
-- link
-- link
-- ...
-
-## More Examples
-
-- link
-- link
-- ...
+![DateRangeBox](sample.png)
---
+++ jQuery/src/index.js
@@ -0,0 +1,73 @@
+$(() => {
+    let startDate = new Date(2022, 5, 18);
+    let endDate = new Date(2022, 5, 23);
+
+    $("#dateRangeBox").dxDropDownBox({
+        value: [startDate, endDate],
+        onValueChanged({ value }) {
+            startDate = value[0];
+            endDate = value[1];
+
+            updateInfo();
+        },
+        dropDownOptions: {
+            width: 324
+        },
+        fieldTemplate(value, fieldElement) {
+            const formatDate = DevExpress.localization.formatDate;
+            const format = "shortDate";
+            const formattedText = value.map(value => formatDate(value, format)).join(" - ");
+
+            const textBox = $("<div>").dxTextBox({
+                readOnly: true,
+                value: formattedText,
+            });
+
+            fieldElement.append(textBox);
+        },
+        contentTemplate({ component }) {
+            const dropDownBox = component;
+            let dateRange = dropDownBox.option("value");
+
+            const $calendar = $("<div>").dxCalendar({
+                value: dateRange[dateRange.length - 1],
+                cellTemplate(cellInfo, index, container) {
+                    const cellDate = cellInfo.date;
+                    dateRange = dropDownBox.option("value");
+
+                    if (cellDate >= dateRange[0] && cellDate <= dateRange[1]) {
+                        container.addClass("dx-calendar-selected-date");
+                    }
+
+                    return $("<span>").text(cellInfo.text);;
+                },
+                onValueChanged({ component, value }) {
+                    const calendar = component;
+                    dateRange = dropDownBox.option("value");
+                    
+                    if (dateRange.length >= 2 || value < dateRange[0]) dateRange = [];
+
+                    dateRange.push(value);
+                    dropDownBox.option("value", dateRange);
+
+                    calendar.repaint();
+                    calendar.focus();
+
+                    if (dateRange.length == 2) {
+                        dropDownBox.close();
+                        dropDownBox.focus();
+                    }
+                },
+            });
+
+            return $calendar;
+        },
+    })
+
+    function updateInfo() {
+        $("#start").html(startDate || "");
+        $("#end").html(endDate || "");
+    }
+
+    updateInfo();
+});
